9e4def45e57bb89c945c1092a5cd9c95b56157bf
Toshiyuki Nishiyama Hiraki <take90-it09-easy27@outlook.jp>
2022-12-17 05:49:50 +0000

    edit and add fucntions to create ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,49 @@
 extern crate rust_ndarray_test;
 
-use ndarray::{array, Array1};
+use ndarray::prelude::*;
 use std::time::Instant;
 use rust_ndarray_test::common::ndarray_init::*;
 
 macro_rules! measure {
-  ( $x:expr) => {
-    {
-      let start = Instant::now();
-      let result = $x;
-      let end = start.elapsed();
-      let elapsed: f64 = end.as_secs() as f64 + end.subsec_micros() as f64 * 1E-6;
-      println!("elapsed time: {} sec.", elapsed);
-      result
-    }
-  };
+    ($x:expr) => {
+        {
+            let start = Instant::now();
+            let result = $x;
+            let end = start.elapsed();
+            let elapsed: f64 = end.as_secs() as f64 + end.subsec_micros() as f64 * 1E-6;
+            println!("elapsed time: {} sec.", elapsed);
+            result
+        }
+    };
 }
 
-fn test_create_array(){
-    // println!("test_create_array");
-    // let start = Instant::now();
-    let  _: Array1<f64> = array![0., 1. ,2.];
-    // let end = start.elapsed();
-    // let elapsed: f64 = end.as_secs() as f64 + end.subsec_micros() as f64 * 1E-6;
-    // println!("{}", a);
-    // println!("elapsed time: {} sec.", elapsed);
+fn test_create_ones_array<D, Sh>(shape: Sh) -> Array<f64, D>
+where D: Dimension, Sh : ShapeBuilder<Dim = D> {
+    Array::<f64, D>::ones(shape)
 }
 
-fn test_create_randomized_ndarray() {
-    println!("test_randomized_ndarray");
-    let n: usize = 10;
+fn test_create_zeros_array<D, Sh>(shape: Sh) -> Array<f64, D>
+where D: Dimension, Sh : ShapeBuilder<Dim = D> {
+    Array::<f64, D>::zeros(shape)
+}
+
+fn test_create_normal_distributed_ndarray<D, Sh>(shape: Sh) -> Array<f64, D>
+where D: Dimension, Sh : ShapeBuilder<Dim = D> {
     let mu: f64 = 1.0;
     let sigma: f64 = 0.5;
-
-    let _: Array1<f64> = initialize_randomized_ndarray(DistributionEnum::Normal, n, &[mu, sigma]);
-    // println!("{}", a);
+    initialize_randomized_ndarray(DistributionEnum::Normal, shape, &[mu, sigma])
 }
 
 fn main() {
-    measure!(test_create_array());
-    measure!(test_create_randomized_ndarray());
+    let shape = (1000, 1000);
+
+    println!("test_create_ones_array");
+    measure!(test_create_ones_array(shape));
+
+    println!("test_create_zeros_array");
+    measure!(test_create_zeros_array(shape));
+
+    println!("test_create_normal_distributed_ndarray");
+    measure!(test_create_normal_distributed_ndarray(shape));
     println!("Hello, world!");
 }
